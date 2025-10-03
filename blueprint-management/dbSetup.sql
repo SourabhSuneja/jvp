235295c4347499ca38fd55db9a3cb03aac8cec44
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-10-03 20:31:59 +0530

    Update dbSetup.sql

--- blueprint-management/dbSetup.sql
+++ blueprint-management/dbSetup.sql
@@ -18,3 +18,33 @@ CREATE POLICY blueprint_authenticated_policy ON blueprint
     FOR ALL
     USING (auth.uid() IS NOT NULL)
     WITH CHECK (auth.uid() IS NOT NULL);
+
+-- Function to allow Blueprint fetching from other apps
+CREATE OR REPLACE FUNCTION get_blueprint_data(p_class INTEGER, p_exam TEXT)
+RETURNS TABLE (
+    class INTEGER,
+    exam TEXT,
+    subject TEXT,
+    blueprint TEXT
+)
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+DECLARE
+    subjects_order TEXT[] := ARRAY[
+        'English', 'Hindi', 'Maths', 'Applied Maths', 'Science', 
+        'Social Science', 'EVS', 'Sanskrit', 'GK', 'Computer', 'Data Science', 'Informatics Practices', 'Physical Education',
+        'Accountancy', 'Business Studies', 'Economics', 'History', 'Geography', 'Political Science',
+        'Psychology','Fine Arts', 'Physics', 'Chemistry', 'Biology'
+    ];
+BEGIN
+    RETURN QUERY
+    SELECT b.class, b.exam, b.subject, b.blueprint
+    FROM blueprint b
+    WHERE b.class = p_class
+      AND b.exam = p_exam
+    ORDER BY 
+        COALESCE(ARRAY_POSITION(subjects_order, b.subject), 9999),  -- custom subject order first
+        b.subject;                                                 -- fallback alphabetical
+END;
+$$;
