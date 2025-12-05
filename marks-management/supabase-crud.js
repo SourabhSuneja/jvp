4eddd824c27385b8001f24d73ea234048ad70bf9
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-12-05 20:31:46 +0530

    Update supabase-crud.js

--- marks-management/supabase-crud.js
+++ marks-management/supabase-crud.js
@@ -1,6 +1,6 @@
 import {
    createClient
-} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.46.1/+esm';
 
 const supabaseUrl = 'https://ckltcwampaagyzneaznt.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbHRjd2FtcGFhZ3l6bmVhem50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MDI2NjgsImV4cCI6MjA0ODA3ODY2OH0.pUT2kngS0nkzFBjI-P6g8azU5E3tZzGDQGL68-AUWFc';
